2ba3c6728abc7efc8d90229a8f9758579d716955
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2021-05-22 10:57:13 +0200

    make the navbar work

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  openOrClose: number = 1
+  ngOnInit() {}
+  toggleNav() {
+    if (this.openOrClose === 1) {
+      this.openOrClose = 2
+    } else {
+      this.openOrClose = 1
+    }
+    console.log(this.openOrClose)
+  }
+  closeNav(){
+    this.openOrClose = 1
+  }
 }
