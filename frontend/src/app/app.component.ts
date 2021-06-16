c20970cdabd5a17ab81784c7d696201bd83a2623
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2021-06-16 18:26:22 +0200

    finsh the about me section

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,19 +6,30 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  duration = () => {
+    let day1 = new Date('2018-9-20');
+    let day2 = new Date();
+
+    let difference = day2.getTime() - day1.getTime();
+    let days = difference / (1000 * 3600 * 24);
+    return Math.floor(days / 365);
+  };
   title = 'frontend';
-  openOrClose: number = 1
-  date = new Date().getFullYear()
+  openOrClose: number = 1;
+  date = new Date().getFullYear();
   ngOnInit() {}
   toggleNav() {
     if (this.openOrClose === 1) {
-      this.openOrClose = 2
+      this.openOrClose = 2;
     } else {
-      this.openOrClose = 1
+      this.openOrClose = 1;
     }
-    console.log(this.openOrClose)
+    console.log(this.openOrClose);
   }
-  closeNav(){
-    this.openOrClose = 1
+  closeNav() {
+    this.openOrClose = 1;
+  }
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
   }
 }
